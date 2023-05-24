3675fc5b6ddfba32b667ac347ab566b37454d847
Karina Serres <karina.serres@gmx.de>
2023-05-24 14:52:18 +0200

    implement rest api with AWS Integration

--- lib/huggingface-ai-deployment.ts
+++ lib/huggingface-ai-deployment.ts
@@ -1,6 +1,6 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
-import { aws_iam as iam, aws_sagemaker as sm, aws_s3 as s3, aws_ecr as ecr } from 'aws-cdk-lib';
-import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
+import { aws_iam as iam, aws_sagemaker as sm, aws_s3 as s3, aws_ecr as ecr, aws_apigateway as apigw } from 'aws-cdk-lib';
+// import * as apigwv2 from "@aws-cdk/aws-apigatewayv2-alpha";
 import { Construct } from 'constructs';
 import * as config from './config';
 import * as sagemaker from '@aws-cdk/aws-sagemaker-alpha';
@@ -76,31 +76,107 @@ export class HuggingfaceAiDeploymentStack extends Stack {
 
     const endpoint = new sagemaker.Endpoint(this, 'Endpoint', { endpointConfig });
 
-    const api = new apigwv2.HttpApi(this, "api", {
-      corsPreflight: {
-        allowHeaders: ["Content-Type"],
-        allowMethods: [
-          apigwv2.CorsHttpMethod.OPTIONS,
-          apigwv2.CorsHttpMethod.POST,
+    const apigwRole = new iam.Role(this, 'ApiGatewayRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+    });
+
+    const apigwPolicy = new iam.Policy(this, 'ApiGatewayPolicy', {
+      statements: [new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "sagemaker:InvokeEndpoint",
         ],
-        allowOrigins: ["*"],
+        resources: [endpoint.endpointArn]
+      })],
+    });
+
+    apigwRole.attachInlinePolicy(apigwPolicy);
+
+    const api = new apigw.RestApi(this, 'ApiGateway', {
+      restApiName: 'ModelApi',
+      description: 'ModelApi',
+      deployOptions: {
+        stageName: 'prod',
+        metricsEnabled: true,
+        loggingLevel: apigw.MethodLoggingLevel.INFO,
       },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: apigw.Cors.DEFAULT_HEADERS,
+      },
+
     });
 
-    const apiStage = new apigwv2.HttpStage(this, "api-stage", {
-      httpApi: api,
-      stageName: "prod",
-      autoDeploy: true,
+    const integration = new apigw.AwsIntegration({
+      service: 'runtime.sagemaker',
+      // action: 'InvokeEndpoint',
+      path: `endpoints/${endpoint.endpointName}/invocations`,
+      integrationHttpMethod: 'POST',
+      options: {
+        credentialsRole: apigwRole,
+        integrationResponses: [
+          {
+            statusCode: '200',
+          },
+        ],
+      }
     });
 
-    const integration = new apigwv2.HttpIntegration(this, "api-integration", {
-      httpApi: api,
-      integrationType: apigwv2.HttpIntegrationType.AWS_PROXY,
-      integrationUri: endpoint.endpointArn
-    });
+    api.root
+      .addMethod('POST', integration);
+
+
+
+
+    //     const integration = new AwsIntegration({
+    //       service: "SageMakerRuntime",
+    //       path: "endpoints/{endpointName}/invocations",
+    //       integrationHttpMethod: "POST",
+    //       options: {
+    //         credentialsRole: Role.fromRoleArn(
+    //           this,
+    //           "Execution role",
+    //           "<arn>"
+    //         )
+    //       }
+    //     });
+
+
+    // const postMethod = api.root
+    //       .addResource(this.node.tryGetContext("serviceVersion"))
+    //       .addResource("endpoints")
+    //       .addResource("{endpointName}")
+    //       .addResource("inferences")
+    //       .addMethod("POST", integration);
+
+
+
+    // const api = new apigwv2.HttpApi(this, "api", {
+    //   corsPreflight: {
+    //     allowHeaders: ["Content-Type"],
+    //     allowMethods: [
+    //       apigwv2.CorsHttpMethod.OPTIONS,
+    //       apigwv2.CorsHttpMethod.POST,
+    //     ],
+    //     allowOrigins: ["*"],
+    //   },
+    // });
+
+    //     new apigwv2.HttpStage(this, "api-stage", {
+    //       httpApi: api,
+    //       stageName: "prod",
+    //       autoDeploy: true,
+    //     });
+
+    // new apigwv2.HttpIntegration(this, "api-integration", {
+    //   httpApi: api,
+    //   integrationType: apigwv2.HttpIntegrationType.AWS_PROXY,
+    //   integrationUri: `endpoints/${endpoint.endpointName}/invocations` //endpoint.endpointArn
+    // });
 
     new CfnOutput(this, 'EndpointName', { value: endpoint.endpointName });
-    new CfnOutput(this, 'ApgwEndpoint', { value: api.apiEndpoint });
+    new CfnOutput(this, 'ApgwEndpoint', { value: api.url });
 
   }
 
