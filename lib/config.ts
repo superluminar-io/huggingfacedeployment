abe21c8eb237c4b603fe6653545a3394a284352b
Kurt Schneider <kurt.schneider@gmail.com>
2023-10-20 16:19:52 +0200

    change config

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,1 @@
-export const LATEST_PYTORCH_VERSION = "1.8.1"
-export const LATEST_TRANSFORMERS_VERSION = "4.10.2"
 export const huggingfaceAccountInfo = { region: 'eu-central-1', account: '763104351884' }
